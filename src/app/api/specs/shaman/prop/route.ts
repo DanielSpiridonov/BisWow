512a97ca8b8497e41468b251626314dd46952e18
i532076 <i532076@fhict.nl>
2025-10-05 16:21:21 +0200

    .

--- src/app/api/specs/shaman/prop/route.ts
+++ src/app/api/specs/shaman/prop/route.ts
@@ -1,8 +1,11 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
-  return NextResponse.json({
-    buildName: "Shaman Prop Build",
-    description: "This is a sample Shaman prop build description.",
-  }, { status: 200 });
+  return NextResponse.json(
+    {
+      buildName: "Shaman Prop Build",
+      description: "This is a sample Shaman prop build description.",
+    },
+    { status: 200 }
+  );
 }
